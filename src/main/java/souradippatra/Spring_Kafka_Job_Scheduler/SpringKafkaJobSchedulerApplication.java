2b2951f77d31ffe88b64c7345568798b6dfa4eb9
Souradip Patra <spectralphoenix.dev@gmail.com>
2025-09-28 17:56:41 +0530

    New Levels, New Devils.

---
+++ src/main/java/souradippatra/Spring_Kafka_Job_Scheduler/SpringKafkaJobSchedulerApplication.java
@@ -0,0 +1,13 @@
+package souradippatra.Spring_Kafka_Job_Scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaJobSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaJobSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/souradippatra/Spring_Kafka_Job_Scheduler/SpringKafkaJobSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package souradippatra.Spring_Kafka_Job_Scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaJobSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
